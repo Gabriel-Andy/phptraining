f570b1e159b893cc8c00fd384958bd8dbb7c875e
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-02-10 08:35:21 -0500

    I learnt how to upload photo inside website

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
-<?php
-include_once "dbh.inc.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>this is where I learn all my lesson</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    
- $sql = "SELECT * FROM users";
- $result = mysqli_query($conn, $sql);
- $datas = array();
- if (mysqli_num_rows($result) > 0 ){
-     
-     while($row = mysqli_fetch_assoc($result)){
-         $datas[]= $row;
-     }
- }
- foreach($datas as $data) {
-     echo $data['usersId'];
- }
-    ?>
-</body>
+    <form action="upload.php" method = "POST" enctype = 'multipart/form-data'>
+<input type ='file' name = 'file'>
+<button type = 'submit' name = 'submit'> upload</button>
+ </form>
+ </body>
 </html>
