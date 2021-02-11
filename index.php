4465a7d9ce94acb15ce7ff50a7355e63b6e9bb15
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-02-11 01:19:22 -0500

    I created an example login section

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+
+<?php
+
+session_start();
+include_once 'dbh.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +12,23 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+if(isset($_SESSION['id'])){
+ 
+}
+?>
     <form action="upload.php" method = "POST" enctype = 'multipart/form-data'>
 <input type ='file' name = 'file'>
 <button type = 'submit' name = 'submit'> upload</button>
  </form>
+ <p>Login as user!</p>
+ <form action = "login.php" method = "POST">
+ <button type = 'submit' name = "submitLogin">Login</button>
+</form>
+
+ <p>Logout as user!</p>
+ <form action = "logout.php" method = "POST">
+ <button type = 'submit' name = "submitLogout">Logout</button>
+</form>
  </body>
 </html>
--- upload.php
+++ upload.php
@@ -8,7 +8,6 @@ $fileTmpName = $_FILES['file']['tmp_name'];
 $fileSize = $_FILES['file']['size'];
 $fileError = $_FILES['file']['error'];
 $fileType = $_FILES['file']['type'];
-
 $fileExt = explode('.', $fileName);
 $fileActualExt = strtolower(end($fileExt));
 $allowed = array('png','jpeg', 'jpg','pdf');
